f97b89a65db433fdfd70437d10fe56cab7e05a9a
sazzad4623 <sazzad111111@gmail.com>
2020-05-08 23:12:49 +0600

    all files added

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyAG-5ZmF1pQXU8yhog9t3UOmlzxw2Acr4U",
+  authDomain: "crown2db-3443d.firebaseapp.com",
+  databaseURL: "https://crown2db-3443d.firebaseio.com",
+  projectId: "crown2db-3443d",
+  storageBucket: "crown2db-3443d.appspot.com",
+  messagingSenderId: "1066989733577",
+  appId: "1:1066989733577:web:c68b4badbc2aed993cef18",
+  measurementId: "G-6C9KJ5QQ6S",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
